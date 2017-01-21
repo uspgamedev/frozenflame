3f7c8496c680d78c043c893682130bdef6865b04
Kazuo256 <kazuo256@gmail.com>
2017-01-21 01:59:33 -0200

    Main menu goes to gameplay

--- game/menu/main.gd
+++ game/menu/main.gd
@@ -1,6 +1,13 @@
+
 extends Control
 
 onready var fader = get_node("Fader")
+onready var transition_to_gameplay = get_node("TransitionToGameplay")
 
 func _ready():
-	fader.fade_in()
+  fader.fade_in()
+  set_process_input(true)
+
+func _input(ev):
+  if ev.is_action_pressed("ui_accept"):
+    transition_to_gameplay.go()
