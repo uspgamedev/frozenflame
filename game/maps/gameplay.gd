abfae78651d9335b33fd2c244f40ac7dcacadd06
Kazuo256 <kazuo256@gmail.com>
2017-01-21 08:09:25 -0200

    Make a simple stage one

--- game/maps/gameplay.gd
+++ game/maps/gameplay.gd
@@ -2,25 +2,23 @@
 extends Node2D
 
 onready var input = get_node("input")
-
-var _hero
+onready var hero = get_node("Map/Bodies/Hero")
 
 func _ready():
-    self._hero = get_node("map/bodies/hero")
     _start_camera()
-    input.connect("hold_direction", self._hero, "_move_to")
+    input.connect("hold_direction", hero, "_move_to")
     input.connect("press_quit", self, "_quit")
-    input.connect("press_action", self._hero, "_act")
-    self._hero.connect("died", self, "_quit")
+    input.connect("press_action", hero, "_act")
+    hero.connect("died", self, "_quit")
     set_process(true)
 
 func _start_camera():
     var cam = Camera2D.new()
-    self._hero.add_child(cam)
+    hero.add_child(cam)
     cam.make_current()
 
 func _quit():
     get_tree().quit()
 
 func get_hero():
-    return self._hero
+    return hero
